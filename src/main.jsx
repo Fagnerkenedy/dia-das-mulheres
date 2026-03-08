dd47c0d7dbfd0ab035331c9e6ade054f0d91e65a
Fagner <fagnerkenedy@hotmail.com>
2026-03-07 22:38:41 -0300

    dia das mulheres

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="min-h-screen w-full flex items-center justify-center bg-pink-50">
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </div>
-);
+  </StrictMode>,
+)
